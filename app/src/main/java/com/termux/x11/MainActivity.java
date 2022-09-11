07237c4286a66b2317f6b36049b8aa822506f6c9
David Kramer <daveskypilot@gmail.com>
2022-09-11 14:06:26 -0500

    Added: Launch fullscreen on external HDMI display

--- app/src/main/java/com/termux/x11/MainActivity.java
+++ app/src/main/java/com/termux/x11/MainActivity.java
@@ -5,11 +5,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Build;
-import android.preference.PreferenceManager;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.KeyEvent;
 import android.view.PointerIcon;
 import android.view.SurfaceView;
@@ -19,9 +16,13 @@ import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.FrameLayout;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.termux.x11.utils.PermissionUtils;
 
 public class MainActivity extends AppCompatActivity {
+    static final String REQUEST_LAUNCH_EXTERNAL_DISPLAY = "request_launch_external_display";
 
     private static int[] keys = {
             KeyEvent.KEYCODE_ESCAPE,
@@ -39,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (didRequestLaunchExternalDisplay()) {
+            setFullScreenForExternalDisplay();
+        }
 
         LorieService.setMainActivity(this);
         LorieService.start(LorieService.ACTION_START_FROM_ACTIVITY);
@@ -62,6 +67,27 @@ public class MainActivity extends AppCompatActivity {
         if (i != null && i.getBooleanExtra(LorieService.LAUNCHED_BY_COMPATION, false)) {
             LorieService.sendRunCommand(this);
         }
+    }
+
+    @Override
+    public void setTheme(int resId) {
+        // for some reason, calling setTheme() in onCreate() wasn't working.
+        super.setTheme(didRequestLaunchExternalDisplay() ?
+                R.style.FullScreen_ExternalDisplay : R.style.NoActionBar);
+    }
+
+    private boolean didRequestLaunchExternalDisplay() {
+        return getIntent().getBooleanExtra(REQUEST_LAUNCH_EXTERNAL_DISPLAY, false);
+    }
+
+    private void setFullScreenForExternalDisplay() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
     }
 
     int orientation;

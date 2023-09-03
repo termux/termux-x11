a26806e0728e971547947807b5f50ba21eba89ce
twaik <twaikyont@gmail.com>
2023-09-03 16:19:09 +0300

    Adding mouse helper overlay.

--- app/src/main/java/com/termux/x11/MainActivity.java
+++ app/src/main/java/com/termux/x11/MainActivity.java
@@ -17,13 +17,17 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.PointF;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Build.VERSION_CODES;
@@ -32,6 +36,7 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.ParcelFileDescriptor;
 import android.os.RemoteException;
+import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.service.notification.StatusBarNotification;
@@ -39,15 +44,19 @@ import android.util.Log;
 import android.view.DragEvent;
 import android.view.InputDevice;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.PointerIcon;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowInsets;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TableLayout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -66,7 +75,11 @@ import com.termux.x11.utils.SamsungDexUtils;
 import com.termux.x11.utils.TermuxX11ExtraKeys;
 import com.termux.x11.utils.X11ToolbarViewPager;
 
+import java.util.Arrays;
+import java.util.Map;
 import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 
 @SuppressLint("ApplySharedPref")
 @SuppressWarnings({"deprecation", "unused"})
@@ -223,6 +236,7 @@ public class MainActivity extends AppCompatActivity implements View.OnApplyWindo
         checkXEvents();
 
         initStylusAuxButtons();
+        initMouseAuxButtons();
 
         if (SDK_INT >= VERSION_CODES.TIRAMISU
                 && checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) != PERMISSION_GRANTED
@@ -294,36 +308,138 @@ public class MainActivity extends AppCompatActivity implements View.OnApplyWindo
         TouchInputHandler.STYLUS_INPUT_HELPER_MODE = 1;
         listener.onClick(left);
 
-        //Enable drag and drop of menu btn
-        frm.setOnDragListener((v, event) -> {
-            //Calculate screen border making sure btn is fully inside the view
-            float maxX = frm.getWidth() - visibility.getWidth();
-            float maxY = frm.getHeight() - visibility.getHeight();
+        visibility.setOnLongClickListener(v -> {
+            v.startDragAndDrop(ClipData.newPlainText("", ""), new View.DragShadowBuilder(visibility) {
+                public void onDrawShadow(Canvas canvas) {}
+            }, null, View.DRAG_FLAG_GLOBAL);
 
-            switch (event.getAction()) {
-                case DragEvent.ACTION_DRAG_LOCATION:
-                    //Center touch location with btn icon
-                    float dX = event.getX() - visibility.getWidth() / 2.0f;
-                    float dY = event.getY() - visibility.getHeight() / 2.0f;
+            frm.setOnDragListener((v2, event) -> {
+                //Calculate screen border making sure btn is fully inside the view
+                float maxX = frm.getWidth() - visibility.getWidth();
+                float maxY = frm.getHeight() - visibility.getHeight();
 
-                    //Make sure the dragged btn is inside the view with clamp
-                    overlay.setX(MathUtils.clamp(dX, 0, maxX));
-                    overlay.setY(MathUtils.clamp(dY, 0, maxY));
-                    break;
-                case DragEvent.ACTION_DRAG_ENDED:
-                    //Make sure the dragged btn is inside the view
-                    overlay.setX(MathUtils.clamp(overlay.getX(), 0, maxX));
-                    overlay.setY(MathUtils.clamp(overlay.getY(), 0, maxY));
-                    break;
-            }
+                switch (event.getAction()) {
+                    case DragEvent.ACTION_DRAG_LOCATION:
+                        //Center touch location with btn icon
+                        float dX = event.getX() - visibility.getWidth() / 2.0f;
+                        float dY = event.getY() - visibility.getHeight() / 2.0f;
+
+                        //Make sure the dragged btn is inside the view with clamp
+                        overlay.setX(MathUtils.clamp(dX, 0, maxX));
+                        overlay.setY(MathUtils.clamp(dY, 0, maxY));
+                        break;
+                    case DragEvent.ACTION_DRAG_ENDED:
+                        //Make sure the dragged btn is inside the view
+                        overlay.setX(MathUtils.clamp(overlay.getX(), 0, maxX));
+                        overlay.setY(MathUtils.clamp(overlay.getY(), 0, maxY));
+                        break;
+                }
+                return true;
+            });
+
             return true;
         });
+    }
 
-        //Activate dragging menu when long pressing visibility btn
-        visibility.setOnLongClickListener(v -> {
-            View.DragShadowBuilder myShadow = new View.DragShadowBuilder(visibility);
-            v.startDragAndDrop(null, myShadow, null, View.DRAG_FLAG_GLOBAL);
-            return true;
+    void setSize(View v, int width, int height) {
+        ViewGroup.LayoutParams p = v.getLayoutParams();
+        p.width = (int) (width * getResources().getDisplayMetrics().density);
+        p.height = (int) (height * getResources().getDisplayMetrics().density);
+        v.setLayoutParams(p);
+        v.setMinimumWidth((int) (width * getResources().getDisplayMetrics().density));
+        v.setMinimumHeight((int) (height * getResources().getDisplayMetrics().density));
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    void initMouseAuxButtons() {
+        Button left = findViewById(R.id.mouse_button_left_click);
+        Button right = findViewById(R.id.mouse_button_right_click);
+        Button middle = findViewById(R.id.mouse_button_middle_click);
+        ImageButton pos = findViewById(R.id.mouse_buttons_position);
+        LinearLayout primaryLayer = findViewById(R.id.mouse_buttons);
+        LinearLayout secondaryLayer = findViewById(R.id.mouse_buttons_secondary_layer);
+
+        SharedPreferences p = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean mouseHelperEnabled = p.getBoolean("showMouseHelper", false);
+        primaryLayer.setVisibility(mouseHelperEnabled ? View.VISIBLE : View.GONE);
+
+        pos.setOnClickListener((v) -> {
+            if (secondaryLayer.getOrientation() == LinearLayout.HORIZONTAL) {
+                setSize(left, 48, 96);
+                setSize(right, 48, 96);
+                secondaryLayer.setOrientation(LinearLayout.VERTICAL);
+            } else {
+                setSize(left, 96, 48);
+                setSize(right, 96, 48);
+                secondaryLayer.setOrientation(LinearLayout.HORIZONTAL);
+            }
+            handler.postDelayed(() -> {
+                int[] offset = new int[2];
+                frm.getLocationOnScreen(offset);
+                primaryLayer.setX(MathUtils.clamp(primaryLayer.getX(), offset[0], offset[0] + frm.getWidth() - primaryLayer.getWidth()));
+                primaryLayer.setY(MathUtils.clamp(primaryLayer.getY(), offset[1], offset[1] + frm.getHeight() - primaryLayer.getHeight()));
+            }, 10);
+        });
+
+        Map.of(left, InputStub.BUTTON_LEFT, middle, InputStub.BUTTON_MIDDLE, right, InputStub.BUTTON_RIGHT).forEach((v, b) -> {
+            v.setOnTouchListener((__, e) -> {
+                switch(e.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                    case MotionEvent.ACTION_POINTER_DOWN:
+                        getLorieView().sendMouseEvent(0, 0, b, true, true);
+                        v.setPressed(true);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_POINTER_UP:
+                        getLorieView().sendMouseEvent(0, 0, b, false, true);
+                        v.setPressed(false);
+                        break;
+                }
+                return true;
+            });
+        });
+
+        pos.setOnTouchListener(new View.OnTouchListener() {
+            final int touchSlop = (int) Math.pow(ViewConfiguration.get(MainActivity.this).getScaledTouchSlop(), 2);
+            final int tapTimeout = ViewConfiguration.getTapTimeout();
+            final float[] startOffset = new float[2];
+            final int[] startPosition = new int[2];
+            long startTime;
+            @Override
+            public boolean onTouch(View v, MotionEvent e) {
+                switch(e.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        primaryLayer.getLocationOnScreen(startPosition);
+                        startOffset[0] = e.getX();
+                        startOffset[1] = e.getY();
+                        startTime = SystemClock.uptimeMillis();
+                        pos.setPressed(true);
+                        break;
+                    case MotionEvent.ACTION_MOVE: {
+                        int[] offset = new int[2];
+                        int[] offset2 = new int[2];
+                        primaryLayer.getLocationOnScreen(offset);
+                        frm.getLocationOnScreen(offset2);
+                        primaryLayer.setX(MathUtils.clamp(offset[0] - startOffset[0] + e.getX(), offset2[0], offset2[0] + frm.getWidth() - primaryLayer.getWidth()));
+                        primaryLayer.setY(MathUtils.clamp(offset[1] - startOffset[1] + e.getY(), offset2[1], offset2[1] + frm.getHeight() - primaryLayer.getHeight()));
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP: {
+                        final int[] _pos = new int[2];
+                        primaryLayer.getLocationOnScreen(_pos);
+                        int deltaX = (int) (startOffset[0] - e.getX()) + (startPosition[0] - _pos[0]);
+                        int deltaY = (int) (startOffset[1] - e.getY()) + (startPosition[1] - _pos[1]);
+                        pos.setPressed(false);
+
+                        if (deltaX * deltaX + deltaY * deltaY < touchSlop && SystemClock.uptimeMillis() - startTime <= tapTimeout) {
+                            v.performClick();
+                            return true;
+                        }
+                        break;
+                    }
+                }
+                return true;
+            }
         });
     }
 
@@ -414,6 +530,7 @@ public class MainActivity extends AppCompatActivity implements View.OnApplyWindo
         if (getRequestedOrientation() != requestedOrientation)
             setRequestedOrientation(requestedOrientation);
 
+        findViewById(R.id.mouse_buttons).setVisibility(p.getBoolean("showMouseHelper", false) ? View.VISIBLE : View.GONE);
         LinearLayout buttons = findViewById(R.id.mouse_helper_visibility);
         if (p.getBoolean("showStylusClickOverride", false)) {
             buttons.setVisibility(View.VISIBLE);
@@ -580,6 +697,7 @@ public class MainActivity extends AppCompatActivity implements View.OnApplyWindo
         setTerminalToolbarView();
     }
 
+    @SuppressLint("WrongConstant")
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);

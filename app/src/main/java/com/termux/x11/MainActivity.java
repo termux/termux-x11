0ba2a80accbf6031a2254f37860f08f559ca2895
Twaik Yont <twaikyont@gmail.com>
2024-03-25 23:07:33 +0200

    Fix broken clipboard behaviour

--- app/src/main/java/com/termux/x11/MainActivity.java
+++ app/src/main/java/com/termux/x11/MainActivity.java
@@ -470,7 +470,7 @@ public class MainActivity extends AppCompatActivity implements View.OnApplyWindo
                 LorieView.connect(fd.detachFd());
                 getLorieView().triggerCallback();
                 clientConnectedStateChanged(true);
-                LorieView.setClipboardSyncEnabled(PreferenceManager.getDefaultSharedPreferences(this).getBoolean("clipboardSync", false));
+                LorieView.setClipboardSyncEnabled(PreferenceManager.getDefaultSharedPreferences(this).getBoolean("clipboardEnable", false));
             } else
                 handler.postDelayed(this::tryConnect, 500);
         } catch (Exception e) {

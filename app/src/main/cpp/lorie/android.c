67abb3c14483a092242910f26b0c64fee1e83c58
Twaik Yont <twaikyont@gmail.com>
2024-04-14 22:41:26 +0300

    Getting rid of absolute mouse. It seems like XTEST protocol handler explicitly uses mouse with only relative axes to send absolute mouse coordinates so I am assuming it is safe.

--- app/src/main/cpp/lorie/android.c
+++ app/src/main/cpp/lorie/android.c
@@ -29,7 +29,7 @@ static int argc = 0;
 static char** argv = NULL;
 static int conn_fd = -1;
 extern char *__progname; // NOLINT(bugprone-reserved-identifier)
-extern DeviceIntPtr lorieMouse, lorieMouseRelative, lorieTouch, lorieKeyboard;
+extern DeviceIntPtr lorieMouse, lorieTouch, lorieKeyboard;
 extern ScreenPtr pScreenPtr;
 extern int ucs2keysym(long ucs);
 void lorieKeysymKeyboardEvent(KeySym keysym, int down);
@@ -331,32 +331,26 @@ void handleLorieEvents(int fd, __unused int ready, __unused void *ignored) {
                 int flags;
                 switch(e.mouse.detail) {
                     case 0: // BUTTON_UNDEFINED
-                        if (e.mouse.relative) {
-                            valuator_mask_set_double(&mask, 0, (double) e.mouse.x);
-                            valuator_mask_set_double(&mask, 1, (double) e.mouse.y);
-                            QueuePointerEvents(lorieMouseRelative, MotionNotify, 0, POINTER_RELATIVE | POINTER_ACCELERATE, &mask);
-                        } else {
-                            flags = POINTER_ABSOLUTE | POINTER_SCREEN | POINTER_NORAW;
-                            valuator_mask_set_double(&mask, 0, (double) e.mouse.x);
-                            valuator_mask_set_double(&mask, 1, (double) e.mouse.y);
-                            QueuePointerEvents(lorieMouse, MotionNotify, 0, flags, &mask);
-                        }
+                        flags = (e.mouse.relative) ? POINTER_RELATIVE | POINTER_ACCELERATE : POINTER_ABSOLUTE | POINTER_SCREEN | POINTER_NORAW;
+                        valuator_mask_set_double(&mask, 0, (double) e.mouse.x);
+                        valuator_mask_set_double(&mask, 1, (double) e.mouse.y);
+                        QueuePointerEvents(lorieMouse, MotionNotify, 0, flags, &mask);
                         break;
                     case 1: // BUTTON_LEFT
                     case 2: // BUTTON_MIDDLE
                     case 3: // BUTTON_RIGHT
-                        QueuePointerEvents(e.mouse.relative ? lorieMouseRelative : lorieMouse, e.mouse.down ? ButtonPress : ButtonRelease, e.mouse.detail, 0, &mask);
+                        QueuePointerEvents(lorieMouse, e.mouse.down ? ButtonPress : ButtonRelease, e.mouse.detail, 0, &mask);
                         break;
                     case 4: // BUTTON_SCROLL
                         if (e.mouse.x) {
                             valuator_mask_zero(&mask);
                             valuator_mask_set_double(&mask, 2, (double) e.mouse.x / 120);
-                            QueuePointerEvents(lorieMouseRelative, MotionNotify, 0, POINTER_RELATIVE, &mask);
+                            QueuePointerEvents(lorieMouse, MotionNotify, 0, POINTER_RELATIVE, &mask);
                         }
                         if (e.mouse.y) {
                             valuator_mask_zero(&mask);
                             valuator_mask_set_double(&mask, 3, (double) e.mouse.y / 120);
-                            QueuePointerEvents(lorieMouseRelative, MotionNotify, 0, POINTER_RELATIVE, &mask);
+                            QueuePointerEvents(lorieMouse, MotionNotify, 0, POINTER_RELATIVE, &mask);
                         }
                         break;
                 }

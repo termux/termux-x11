15993a6c227bd536013832719263c712d5b7ad9e
Twaik Yont <9674930+twaik@users.noreply.github.com>
2026-07-27 23:04:04 +0300

    chore(lorie): drop input tracing left over from debugging
    InputConnectionWrapper only logged every call and delegated it, and the EditText subclass
    existed solely to install it, so both are gone along with the cursor position and stylus
    event logs.

--- lorie/src/main/java/com/termux/x11/LorieView.java
+++ lorie/src/main/java/com/termux/x11/LorieView.java
@@ -66,271 +66,6 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import dalvik.annotation.optimization.CriticalNative;
 import dalvik.annotation.optimization.FastNative;
 
-class InputConnectionWrapper implements InputConnection {
-    private static final String TAG = "InputConnectionWrapper";
-    private final InputConnection wrapped;
-
-    public InputConnectionWrapper(InputConnection wrapped) {
-        this.wrapped = wrapped;
-    }
-
-    @Override
-    public CharSequence getTextBeforeCursor(int n, int flags) {
-        Log.d(TAG, "getTextBeforeCursor(" + n + ", " + flags + ")");
-        return wrapped.getTextBeforeCursor(n, flags);
-    }
-
-    @Override
-    public CharSequence getTextAfterCursor(int n, int flags) {
-        Log.d(TAG, "getTextAfterCursor(" + n + ", " + flags + ")");
-        return wrapped.getTextAfterCursor(n, flags);
-    }
-
-    @Override
-    public CharSequence getSelectedText(int flags) {
-        Log.d(TAG, "getSelectedText(" + flags + ")");
-        return wrapped.getSelectedText(flags);
-    }
-
-    @Override
-    public SurroundingText getSurroundingText(int beforeLength, int afterLength, int flags) {
-        Log.d(TAG, "getSurroundingText(" + beforeLength + ", " + afterLength + ", " + flags + ")");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            return wrapped.getSurroundingText(beforeLength, afterLength, flags);
-        } else return null;
-    }
-
-    @Override
-    public int getCursorCapsMode(int reqModes) {
-        Log.d(TAG, "getCursorCapsMode(" + reqModes + ")");
-        return wrapped.getCursorCapsMode(reqModes);
-    }
-
-    @Override
-    public ExtractedText getExtractedText(ExtractedTextRequest request, int flags) {
-        Log.d(TAG, "getExtractedText(" + request + ", " + flags + ")");
-        return wrapped.getExtractedText(request, flags);
-    }
-
-    @Override
-    public boolean deleteSurroundingText(int beforeLength, int afterLength) {
-        Log.d(TAG, "deleteSurroundingText(" + beforeLength + ", " + afterLength + ")");
-        return wrapped.deleteSurroundingText(beforeLength, afterLength);
-    }
-
-    @Override
-    public boolean deleteSurroundingTextInCodePoints(int beforeLength, int afterLength) {
-        Log.d(TAG, "deleteSurroundingTextInCodePoints(" + beforeLength + ", " + afterLength + ")");
-        return wrapped.deleteSurroundingTextInCodePoints(beforeLength, afterLength);
-    }
-
-    @Override
-    public boolean setComposingText(CharSequence text, int newCursorPosition) {
-        Log.d(TAG, "setComposingText(" + text + ", " + newCursorPosition + ")");
-        return wrapped.setComposingText(text, newCursorPosition);
-    }
-
-    @Override
-    public boolean setComposingText(@NonNull CharSequence text, int newCursorPosition, TextAttribute textAttribute) {
-        Log.d(TAG, "setComposingText(" + text + ", " + newCursorPosition + ", " + textAttribute + ")");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            return wrapped.setComposingText(text, newCursorPosition, textAttribute);
-        } else return false;
-    }
-
-    @Override
-    public boolean setComposingRegion(int start, int end) {
-        Log.d(TAG, "setComposingRegion(" + start + ", " + end + ")");
-        return wrapped.setComposingRegion(start, end);
-    }
-
-    @Override
-    public boolean setComposingRegion(int start, int end, TextAttribute textAttribute) {
-        Log.d(TAG, "setComposingRegion(" + start + ", " + end + ", " + textAttribute + ")");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            return wrapped.setComposingRegion(start, end, textAttribute);
-        } else return false;
-    }
-
-    @Override
-    public boolean finishComposingText() {
-        Log.d(TAG, "finishComposingText()");
-        return wrapped.finishComposingText();
-    }
-
-    @Override
-    public boolean commitText(CharSequence text, int newCursorPosition) {
-        Log.d(TAG, "commitText(" + text + ", " + newCursorPosition + ")");
-        return wrapped.commitText(text, newCursorPosition);
-    }
-
-    @Override
-    public boolean commitText(@NonNull CharSequence text, int newCursorPosition, TextAttribute textAttribute) {
-        Log.d(TAG, "commitText(" + text + ", " + newCursorPosition + ", " + textAttribute + ")");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            return wrapped.commitText(text, newCursorPosition, textAttribute);
-        } else return false;
-    }
-
-    @Override
-    public boolean commitCompletion(CompletionInfo text) {
-        Log.d(TAG, "commitCompletion(" + text + ")");
-        return wrapped.commitCompletion(text);
-    }
-
-    @Override
-    public boolean commitCorrection(CorrectionInfo correctionInfo) {
-        Log.d(TAG, "commitCorrection(" + correctionInfo + ")");
-        return wrapped.commitCorrection(correctionInfo);
-    }
-
-    @Override
-    public boolean setSelection(int start, int end) {
-        Log.d(TAG, "setSelection(" + start + ", " + end + ")");
-        return wrapped.setSelection(start, end);
-    }
-
-    @Override
-    public boolean performEditorAction(int editorAction) {
-        Log.d(TAG, "performEditorAction(" + editorAction + ")");
-        return wrapped.performEditorAction(editorAction);
-    }
-
-    @Override
-    public boolean performContextMenuAction(int id) {
-        Log.d(TAG, "performContextMenuAction(" + id + ")");
-        return wrapped.performContextMenuAction(id);
-    }
-
-    @Override
-    public boolean beginBatchEdit() {
-        Log.d(TAG, "beginBatchEdit()");
-        return wrapped.beginBatchEdit();
-    }
-
-    @Override
-    public boolean endBatchEdit() {
-        Log.d(TAG, "endBatchEdit()");
-        return wrapped.endBatchEdit();
-    }
-
-    @Override
-    public boolean sendKeyEvent(KeyEvent event) {
-        Log.d(TAG, "sendKeyEvent(" + event + ")");
-        return wrapped.sendKeyEvent(event);
-    }
-
-    @Override
-    public boolean clearMetaKeyStates(int states) {
-        Log.d(TAG, "clearMetaKeyStates(" + states + ")");
-        return wrapped.clearMetaKeyStates(states);
-    }
-
-    @Override
-    public boolean reportFullscreenMode(boolean enabled) {
-        Log.d(TAG, "reportFullscreenMode(" + enabled + ")");
-        return wrapped.reportFullscreenMode(enabled);
-    }
-
-    @Override
-    public boolean performSpellCheck() {
-        Log.d(TAG, "performSpellCheck()");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            return wrapped.performSpellCheck();
-        } else return false;
-    }
-
-    @Override
-    public boolean performPrivateCommand(String action, Bundle data) {
-        Log.d(TAG, "performPrivateCommand(" + action + ", " + data + ")");
-        return wrapped.performPrivateCommand(action, data);
-    }
-
-    @Override
-    public void performHandwritingGesture(@NonNull HandwritingGesture gesture, Executor executor, IntConsumer consumer) {
-        Log.d(TAG, "performHandwritingGesture(" + gesture + ", " + executor + ", " + consumer + ")");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-            wrapped.performHandwritingGesture(gesture, executor, consumer);
-        }
-    }
-
-    @Override
-    public boolean previewHandwritingGesture(@NonNull PreviewableHandwritingGesture gesture, CancellationSignal cancellationSignal) {
-        Log.d(TAG, "previewHandwritingGesture(" + gesture + ", " + cancellationSignal + ")");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-            return wrapped.previewHandwritingGesture(gesture, cancellationSignal);
-        } else return false;
-    }
-
-    @Override
-    public boolean requestCursorUpdates(int cursorUpdateMode) {
-        Log.d(TAG, "requestCursorUpdates(" + cursorUpdateMode + ")");
-        return wrapped.requestCursorUpdates(cursorUpdateMode);
-    }
-
-    @Override
-    public boolean requestCursorUpdates(int cursorUpdateMode, int cursorUpdateFilter) {
-        Log.d(TAG, "requestCursorUpdates(" + cursorUpdateMode + ", " + cursorUpdateFilter + ")");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            return wrapped.requestCursorUpdates(cursorUpdateMode, cursorUpdateFilter);
-        } else return false;
-    }
-
-    @Override
-    public void requestTextBoundsInfo(@NonNull RectF bounds, @NonNull Executor executor, @NonNull Consumer<TextBoundsInfoResult> consumer) {
-        Log.d(TAG, "requestTextBoundsInfo(" + bounds + ", " + executor + ", " + consumer + ")");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-            wrapped.requestTextBoundsInfo(bounds, executor, consumer);
-        }
-    }
-
-    @Override
-    public Handler getHandler() {
-        Log.d(TAG, "getHandler()");
-        return wrapped.getHandler();
-    }
-
-    @Override
-    public void closeConnection() {
-        Log.d(TAG, "closeConnection()");
-        wrapped.closeConnection();
-    }
-
-    @Override
-    public boolean commitContent(@NonNull InputContentInfo inputContentInfo, int flags, Bundle opts) {
-        Log.d(TAG, "commitContent(" + inputContentInfo + ", " + flags + ", " + opts + ")");
-        return wrapped.commitContent(inputContentInfo, flags, opts);
-    }
-
-    @Override
-    public boolean setImeConsumesInput(boolean imeConsumesInput) {
-        Log.d(TAG, "setImeConsumesInput(" + imeConsumesInput + ")");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            return wrapped.setImeConsumesInput(imeConsumesInput);
-        } else return false;
-    }
-
-    @Override
-    public TextSnapshot takeSnapshot() {
-        Log.d(TAG, "takeSnapshot()");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            return wrapped.takeSnapshot();
-        } else return null;
-    }
-
-    @Override
-    public boolean replaceText(int start,
-                               int end,
-                               @NonNull CharSequence text,
-                               int newCursorPosition,
-                               TextAttribute textAttribute) {
-        Log.d(TAG, "replaceText(" + start + ", " + end + ", " + text + ", " + newCursorPosition + ", " + textAttribute + ")");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-            return wrapped.replaceText(start, end, text, newCursorPosition, textAttribute);
-        } else return false;
-    }
-}
-
 @Keep @SuppressLint("WrongConstant")
 @SuppressWarnings("deprecation")
 public class LorieView extends SurfaceView implements InputStub {
@@ -358,7 +93,7 @@ public class LorieView extends SurfaceView implements InputStub {
     private float inputSourceLeft = 0.f, inputSourceTop = 0.f;
     private float inputSourceWidth = 0.f, inputSourceHeight = 0.f;
     boolean commitedText = false;
-    private final InputConnection mConnection = new InputConnectionWrapper(new BaseInputConnection(this, false) {
+    private final InputConnection mConnection = new BaseInputConnection(this, false) {
         private final MainActivity a = MainActivity.getInstance();
         private CharSequence currentComposingText = null;
 
@@ -414,7 +149,6 @@ public class LorieView extends SurfaceView implements InputStub {
                 currentPos = 1;
             }
             mIMM.updateSelection(LorieView.this, currentPos, currentPos, -1, -1);
-            Log.d("InputConnectionWrapper", "SENDING CURSOR POS " + currentPos);
         }
 
         // Needed to send arrow keys with IME's cursor control feature
@@ -525,8 +259,6 @@ public class LorieView extends SurfaceView implements InputStub {
 
         @Override
         public boolean commitText(CharSequence text, int newPos) {
-            Log.d("InputConnectionWrapper", newPos + " - 1 + " + currentPos + " + " + text.length());
-            Log.d("InputConnectionWrapper", "OLD " + currentPos + " NEW " + Math.max(1, newPos - 1 + currentPos + text.length()) + " mBatchEditNesting " + mBatchEditNesting);
             if (newPos > 0)
                 currentPos = Math.max(1, newPos - 1 + currentPos + text.length());
             else
@@ -563,7 +295,7 @@ public class LorieView extends SurfaceView implements InputStub {
         public boolean requestCursorUpdates(int cursorUpdateMode, int cursorUpdateFilter) {
             return requestCursorUpdates(cursorUpdateMode);
         }
-    });
+    };
     private final SurfaceHolder.Callback mSurfaceCallback = new SurfaceHolder.Callback() {
         @Override public void surfaceCreated(@NonNull SurfaceHolder holder) {
             holder.setFormat(PixelFormat.BGRA_8888);
--- lorie/src/main/java/com/termux/x11/input/TouchInputHandler.java
+++ lorie/src/main/java/com/termux/x11/input/TouchInputHandler.java
@@ -979,7 +979,6 @@ public class TouchInputHandler {
                 tiltY = (int) Math.round((float) Math.asin( Math.cos(orientation) * Math.sin(tilt)) * 63.5 - 0.5);
             }
 
-            android.util.Log.d("STYLUS_EVENT", "action " + action + " x " + newX + " y " + newY + " pressure " + e.getPressure() + " tilt " + e.getAxisValue(MotionEvent.AXIS_TILT) + " orientation " + e.getAxisValue(MotionEvent.AXIS_ORIENTATION) + " buttonState " + e.getButtonState() + " extractedButtons " + newButtons);
             mInjector.sendStylusEvent(
                     x = newX,
                     y = newY,
